21626f5c09f6d9352756b26dc6baa2d551219faf
Michele Locati <michele@locati.it>
2017-06-21 08:42:13 +0200

    Use native TextDecoder/TextEncoder if available

--- src/Gettext/Charset.ts
+++ src/Gettext/Charset.ts
@@ -5,8 +5,51 @@ export namespace Gettext {
      * Helper class to work with strings.
      */
     export class Charset {
-        private static decoders: { [id: string]: encoding.TextDecoder } = {};
-        private static encoder: encoding.TextEncoder;
+        private static _useNative: boolean;
+        private static useNative(): boolean {
+            if (Charset._useNative === undefined) {
+                debugger;
+                Charset._useNative = false;
+                if (window !== undefined) {
+                    let w = <any>window;
+                    if (w.TextDecoder && w.TextDecoder.prototype && w.TextEncoder && w.TextEncoder.prototype) {
+                        Charset._useNative = true;
+                    }
+                }
+            }
+            return Charset._useNative;
+        }
+
+        private static _decoders: { [id: string]: encoding.TextDecoder } = {};
+        private static getDecoder(charset: string): encoding.TextDecoder {
+            charset = charset ? charset.toLocaleLowerCase() : '';
+            if (Charset._decoders.hasOwnProperty(charset) === false) {
+                let ctor: encoding.TextDecoderStatic;
+                if (Charset.useNative()) {
+                    ctor = <encoding.TextDecoderStatic>(<any>window).TextDecoder;
+                } else {
+                    ctor = encoding.TextDecoder;
+                }
+
+                Charset._decoders[charset] = new ctor(charset, { fatal: true });
+            }
+            return Charset._decoders[charset];
+        }
+
+        private static _encoder: encoding.TextEncoder;
+        private static getEncoder(): encoding.TextEncoder {
+            if (Charset._encoder === undefined) {
+                let ctor: encoding.TextEncoderStatic;
+                if (Charset.useNative()) {
+                    ctor = <encoding.TextEncoderStatic>(<any>window).TextEncoder;
+                } else {
+                    ctor = encoding.TextEncoder;
+                }
+                Charset._encoder = new ctor();
+            }
+            return Charset._encoder;
+        }
+
         public static bufferToString(buffer: ArrayBuffer, charset: string, byteOffset?: number, numberOfBytes?: number): string {
             byteOffset = byteOffset || 0;
             if (numberOfBytes === undefined) {
@@ -18,21 +61,16 @@ export namespace Gettext {
             if (numberOfBytes < 0 || numberOfBytes + byteOffset > buffer.byteLength) {
                 throw new Error('Invalid number of bytes to convert to string');
             }
-            charset = charset ? charset.toLocaleLowerCase() : '';
+            let decoder = Charset.getDecoder(charset);
             let bytes = new Uint8Array(buffer, byteOffset, numberOfBytes);
-
-            if (!Charset.decoders.hasOwnProperty(charset)) {
-                Charset.decoders[charset] = new encoding.TextDecoder(charset, { fatal: true });
-            }
-            let decoder: encoding.TextDecoder = Charset.decoders[charset];
             return decoder.decode(bytes);
         }
+
         public static stringToUtf8Uint8Array(string: string): Uint8Array {
-            if (Charset.encoder === undefined) {
-                Charset.encoder = new encoding.TextEncoder();
-            }
-            return Charset.encoder.encode(string);
+            let encoder = Charset.getEncoder();
+            return encoder.encode(string);
         }
+
         public static stringToUtf8Blob(string: string, contentType: string): Blob {
             return new Blob([string], { type: contentType, endings: "transparent" });
         }
